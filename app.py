2a60678138929accdb40c839af8bfa528c6e8e36
Priyashanmugasundaram005 <priyashanmugasundaram2005@gmail.com>
2025-10-13 18:21:54 +0530

    Deploy: add requirements.txt, Procfile, and DB init hook for Gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import db
+from models import db, init_default_locations
 from views import product_bp, location_bp, movement_bp
 
 app = Flask(__name__)
@@ -10,12 +10,16 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 # Init DB
 db.init_app(app)
 
+@app.before_first_request
+def ensure_db_initialized():
+    with app.app_context():
+        db.create_all()
+        init_default_locations()
+
 # Register Blueprints
 app.register_blueprint(product_bp)
 app.register_blueprint(location_bp)
 app.register_blueprint(movement_bp)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()   # create tables if not exist
     app.run(debug=True, host='0.0.0.0', port=8089)
